5eae8ccc3fcd8157ad2b0a24ce09d979b650298f
Philipp Kitzberger <coding@kitze.net>
2022-12-30 06:55:36 +0100

    FIX: songs.url doesn't have a default value

---
+++ config/Migrations/20221230064912_NullableFieldsInSongs.php
@@ -0,0 +1,20 @@
+<?php
+
+use Migrations\AbstractMigration;
+
+class NullableFieldsInSongs extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('songs');
+        $table->changeColumn('url', 'string', ['default' => null, 'limit' => 255, 'null' => true]);
+        $table->update();
+    }
+}
